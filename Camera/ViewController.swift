c842c1cf64700c959db6b9cac2ab2fab6609fac7
👨🏾‍💻 Henry AT <yiperu@gmail.com>
2020-06-25 15:53:37 -0400

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by everis on 25-06-20.
+//  Copyright © 2020 everis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
